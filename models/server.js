76a83d2003b2a80f68bd33d8bf31970e1a5d9ed6
EmmanuelZuluaga <emmanuel.zuluaga@imagineapps.co>
2022-09-09 19:32:54 -0500

    create schema node

---
+++ models/server.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+
+const { dbConnection } = require('../database/config');
+const bodyParser = require('body-parser');
+
+class Server {
+  constructor() {
+    
+    this.app = express();
+    this.app.use(bodyParser.json({ limit: '500mb', extended: true }));
+    this.app.use(bodyParser.urlencoded({ limit: '500mb', extended: true }));
+    this.app.disable('x-powered-by');
+    this.app.use(express.urlencoded({ extended: true }));
+    this.port = process.env.PORT;
+    this.paths = {
+    //  auth: '/api/auth',
+     // users: '/api/users',
+    };
+    //Conectar a DB
+   // this.connectDB();
+    //Middlewares
+   // this.middlewares();
+    //Rutas de mi aplicación
+    this.routes();
+  }
+
+  async connectDB() {
+    await dbConnection();
+  }
+
+  middlewares() {
+    //CORS
+    this.app.use(cors());
+    //Parse y lectura del body
+    this.app.use(express.json());
+    //Directorio público
+    this.app.use(express.static('public'));
+  }
+
+  routes() {
+  //  this.app.use(this.paths.auth, require('../routes/auth'));
+   // this.app.use(this.paths.users, require('../routes/user'));
+
+    this.app.get('***', (req, res) => {
+      res.sendFile(path.join(__dirname, 'public/index.html'));
+    });
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log('Servidor corriendo en puerto', this.port);
+    });
+  }
+}
+
+module.exports = Server;
---
+++ routes/auth.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const { check } = require('express-validator');
+const { login, validateUserToken, recoverPassword, changePassword, loginGoogle } = require('../controllers/auth');
+const { validateFields } = require('../middlewares/validate-fields');
+const { validateJWT } = require('../middlewares/validate-jwt');
+
+const router = express();
+router.disable('x-powered-by');
+
+//Login with Google
+router.post('/google', loginGoogle);
+
+//Login
+router.post(
+  '/login',
+  [
+    check('email', 'Error in email Field').isEmail(),
+    check('password', 'Password is required').not().isEmpty(),
+    validateFields,
+  ],
+  login
+);
+
+router.get('/', [validateJWT], validateUserToken);
+
+router.put('/changePassword/:id', changePassword);
+
+// Route recover password
+router.post(
+  '/recoverPassword',
+  [
+    check('email', 'Formato incorrecto de email').isEmail(),
+    check('email', 'El email es obligatorio').not().isEmpty(),
+    validateFields,
+  ],
+  recoverPassword
+);
+
+module.exports = router;
